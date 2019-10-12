ef1328dd648d151cdef4175a3c4b5d047b479782
Mikhail Nakhatovich <mish_1998@mail.ru>
2019-10-12 19:52:49 +0300

    Added new constants to utils
    - urls, config for authorization

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,3 +1,5 @@
+import { getSession } from '../cookieManager';
+
 export const RESTART_ON_REMOUNT = '@@saga-injector/restart-on-remount';
 export const DAEMON = '@@saga-injector/daemon';
 export const ONCE_TILL_UNMOUNT = '@@saga-injector/once-till-unmount';
@@ -13,3 +15,21 @@ export const serverUrl = 'http://80.78.255.236:1337';
 export const requestDataUrl = `${serverUrl}/graphql`;
 
 export const CAPTCHA_KEY = '6LfH7KsUAAAAAE9bCsjxT03vsKFuEmNwytHjyTsX';
+
+export const server = 'http://localhost:4200/';
+
+export const urls = {
+  auth: {
+    authorization_url: `${server}oauth2/authorize`,
+    signup_url: `${server}oauth2/register`,
+    access_token_url: `${server}oauth2/token`,
+    token_info_url: `${server}oauth2/info?access_token=${getSession()}`,
+    revoke_token_url: `${server}oauth2/revoke`,
+    oauth2RedirectUrl: '/auth',
+  },
+};
+
+export const auth_config = {
+  client_id: 'V0XVzw07BSpSIwGQOuhASxFo',
+  scopes: ['user'],
+};
