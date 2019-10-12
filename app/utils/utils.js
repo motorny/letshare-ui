a7bd6dd282d57a71126b1128385f078f0752c5ee
Mikhail Nakhatovich <mish_1998@mail.ru>
2019-10-12 19:53:45 +0300

    Added `parseQueryString` function to utils

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -19,3 +19,17 @@ export function post(url, params, callback) {
     })
     .then(callback);
 }
+
+export const parseQueryString = str => {
+  if (!str) return {};
+  const arr = str.split('&');
+  arr.forEach((v, i, _arr) => {
+    _arr[i] = `"${v.replace('=', '":"')}"`;
+  });
+  return str
+    ? JSON.parse(
+      `{${arr.join()}}`,
+      (key, value) => (key === '' ? value : decodeURIComponent(value)),
+    )
+    : {};
+};
