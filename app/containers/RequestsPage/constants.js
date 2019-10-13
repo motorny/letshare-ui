8ca716cd2ab6283c0f44f0e336abb710b22a0d53
chopikov <ilyacuri@gmail.com>
2019-10-13 03:02:38 +0300

    Fixed constants names

--- app/containers/RequestsPage/constants.js
+++ app/containers/RequestsPage/constants.js
@@ -5,6 +5,6 @@
  * export const YOUR_ACTION_CONSTANT = 'yourproject/YourContainer/YOUR_ACTION_CONSTANT';
  */
 
-export const LOAD_MAIN = 'its/NewsPage/LOAD_MAIN';
-export const LOAD_MAIN_SUCCESS = 'its/NewsPage/LOAD_MAIN_SUCCESS';
-export const LOAD_MAIN_ERROR = 'its/NewsPage/LOAD_MAIN_ERROR';
+export const LOAD_MAIN = 'its/RequestsPage/LOAD_MAIN';
+export const LOAD_MAIN_SUCCESS = 'its/RequestsPage/LOAD_MAIN_SUCCESS';
+export const LOAD_MAIN_ERROR = 'its/RequestsPage/LOAD_MAIN_ERROR';
